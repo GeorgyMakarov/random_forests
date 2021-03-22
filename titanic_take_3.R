9137faa5ced7163df7d1c29cd51a3e1a651b431d
Georgy Makarov <60594767+GeorgyMakarov@users.noreply.github.com>
2021-03-22 17:27:04 +0100

    add family to model

--- titanic_take_3.R
+++ titanic_take_3.R
@@ -3,7 +3,6 @@
 # learn random forest tuning and acheive maximum possible score.
 setwd("/home/georgy/Документы/Rstudio/sreda_solutions/interview_tasks/task_3")
 suppressPackageStartupMessages(library(dplyr))
-suppressPackageStartupMessages(library(rpart))
 suppressPackageStartupMessages(library(ROSE))
 suppressPackageStartupMessages(library(randomForest))
 suppressPackageStartupMessages(library(ranger))
@@ -61,23 +60,64 @@ rm(temp_cor)
 all_data$Age = round(all_data$Age, 0)
 
 
+# Add title as a factor
+# Convert titles to men, women, kids
+all_data$Title <- sapply(all_data$Name, 
+                         FUN=function(x) {strsplit(x, split='[,.]')[[1]][2]})
+all_data$Title = sub(' ', '', all_data$Title)
+men   = c("Capt", "Don", "Major", "Col", "Rev", "Dr", "Sir", "Mr", "Jonkheer")
+women = c("Dona", "the Countess", "Mme", "Mlle", "Ms", "Miss", "Lady", "Mrs")
+all_data$Title[all_data$Title %in% men]      = "man"
+all_data$Title[all_data$Title %in% women]    = "woman"
+all_data$Title[all_data$Title %in% "Master"] = "boy"
+rm(men, women)
+
+
+# Extract surnames to make child-woman groups
+# Compute family survival rates
+all_data$Surname = substring(all_data$Name, 0, regexpr(",",all_data$Name)-1)
+all_data$Surname[all_data$Title == 'man'] = 'noGroup'
+all_data$SurnameFreq = ave(1:1309,all_data$Surname,FUN=length)
+all_data$Surname[all_data$SurnameFreq <= 1] = 'noGroup'
+
+all_data$SurnameSurvival = NA
+all_data$SurnameSurvival[1:891] = ave(all_data$Survived[1:891],
+                                      all_data$Surname[1:891])
+
+for (i in 892:1309){
+    all_data$SurnameSurvival[i] =
+        all_data$SurnameSurvival[
+            which(all_data$Surname == all_data$Surname[i])[1]]
+}
+rm(i)
+
+
+all_data$SurnameSurvival[is.na(all_data$SurnameSurvival)] = 0.5
+summary(all_data)
+
+
 # The data shows that 52% of the boys under 16 survived
 # Make a feature that separates boys from other men
 all_data$IsBoy = "no"
 all_data$IsBoy[all_data$Sex == "male" & all_data$Age < 16] = "yes"
 
 
-# Add title as a factor
-all_data$Title <- sapply(all_data$Name, 
-                         FUN=function(x) {strsplit(x, split='[,.]')[[1]][2]})
-all_data$Title = sub(' ', '', all_data$Title)
-all_data$Title[all_data$Title %in% c('Mme', 'Mlle')]  = 'Mlle'
-all_data$Title[all_data$Title %in% c('Capt', 'Don', 
-                                     'Major', 'Sir')] = 'Sir'
-all_data$Title[all_data$Title %in% c('Dona', 'Lady', 
-                                     'the Countess', 'Jonkheer')] = 'Lady'
+# Add family size
+all_data$FamSize = all_data$SibSp + all_data$Parch
 
 
+# Add official title
+all_data$Official <- sapply(all_data$Name, 
+                            FUN=function(x) {strsplit(x, split='[,.]')[[1]][2]})
+all_data$Official = sub(' ', '', all_data$Official)
+mils = c("Capt", "Major", "Col", "Sir")
+mmle = c("Mme", "Mlle")
+lads = c("Dona", "the Countess", "Lady")
+
+all_data$Official[all_data$Official %in% mils] = "Sir"
+all_data$Official[all_data$Official %in% mmle] = "Mmle"
+all_data$Official[all_data$Official %in% lads] = "Lady"
+rm(mils, mmle, lads)
 
 
 # Make model --------------------------------------------------------------
@@ -85,13 +125,15 @@ all_data$Title[all_data$Title %in% c('Dona', 'Lady',
 # select features
 sel_data = 
     all_data %>% 
-    select(Survived, Sex, Pclass, Embarked, IsBoy, Title, Segm)
+    select(Survived, Sex, Pclass, Embarked, Surname, 
+           SurnameSurvival, Official, Segm)
 
 sel_data$Sex       = as.factor(sel_data$Sex)
 sel_data$Pclass    = as.factor(sel_data$Pclass)
 sel_data$Embarked  = as.factor(sel_data$Embarked)
-sel_data$IsBoy     = as.factor(sel_data$IsBoy)
-sel_data$Title     = as.factor(sel_data$Title)
+sel_data$Surname   = as.factor(sel_data$Surname)
+sel_data$Official  = as.factor(sel_data$Official)
+
 
 training = sel_data %>% filter(Segm == "training") %>% select(-Segm)
 testing  = sel_data %>% filter(Segm == "testing") %>% select(-Segm)
@@ -101,7 +143,7 @@ training$Survived = factor(ifelse(training$Survived == 0, "no", "yes"),
 rm(sel_data)
 
 
-hyper_grid = expand.grid(mtry        = seq(1, ncol(training) - 1, by = 1), 
+hyper_grid = expand.grid(mtry        = seq(2, ncol(training) - 1, by = 1), 
                          node_size   = seq(2, 15, by = 2), 
                          sample_size = c(0.55, 0.632, 0.70, 0.80), 
                          OOB_RMSE    = 0)
@@ -147,3 +189,15 @@ output      = output %>% select(obs, pred, yes = pred.yes, no = pred.no)
 confusionMatrix(data = output$pred, reference = output$obs)
 twoClassSummary(data = output, lev  = levels(output$obs))
 rm(output, pred)
+
+
+
+
+
+
+
+
+
+
+
+
